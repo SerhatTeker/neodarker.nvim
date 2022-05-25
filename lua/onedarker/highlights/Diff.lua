0ddefd97f0da075fdb08f4e5bcc3cd1ae89f7ec5
Serhat Teker <serhat.teker@gmail.com>
2022-05-25 17:00:24 +0300

    Change Diff colors to sign colors

--- lua/onedarker/highlights/Diff.lua
+++ lua/onedarker/highlights/Diff.lua
@@ -1,9 +1,9 @@
 local C = require "onedarker.palette"
 
 local Diff = {
-  DiffAdd = { fg = C.none, bg = C.diff_add },
-  DiffDelete = { fg = C.none, bg = C.diff_delete },
-  DiffChange = { fg = C.none, bg = C.diff_change, style = "bold" },
+  DiffAdd = { fg = C.none, bg = C.sign_add },
+  DiffDelete = { fg = C.none, bg = C.sign_delete },
+  DiffChange = { fg = C.none, bg = C.sign_change, style = "bold" },
   DiffText = { fg = C.none, bg = C.diff_text },
   DiffAdded = { fg = C.green },
   DiffRemoved = { fg = C.red },
