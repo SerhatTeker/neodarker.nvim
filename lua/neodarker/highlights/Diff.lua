aea52415598acc54b6601b2849c36c45e7f244e3
Serhat Teker <serhat.teker@gmail.com>
2022-06-17 16:21:28 +0300

    Revert back Diff highlights

--- lua/neodarker/highlights/Diff.lua
+++ lua/neodarker/highlights/Diff.lua
@@ -1,14 +1,18 @@
 local C = require "neodarker.palette"
 
 local Diff = {
-  DiffAdd = { fg = C.none, bg = C.sign_add },
-  DiffDelete = { fg = C.none, bg = C.sign_delete },
-  DiffChange = { fg = C.none, bg = C.sign_change, style = "bold" },
-  DiffText = { fg = C.none, bg = C.diff_text },
-  DiffAdded = { fg = C.green },
-  DiffRemoved = { fg = C.red },
-  DiffFile = { fg = C.cyan },
-  DiffIndexLine = { fg = C.gray },
+    -- -- TODO: Not working in Alacritty. Alternatives:
+    -- DiffAdd = { fg = C.none, bg = C.sign_add },
+    -- DiffDelete = { fg = C.none, bg = C.sign_delete },
+    -- DiffChange = { fg = C.none, bg = C.sign_change, style = "bold" },
+    DiffAdd = { fg = C.none, bg = C.diff_add },
+    DiffDelete = { fg = C.none, bg = C.diff_delete },
+    DiffChange = { fg = C.none, bg = C.diff_change, style = "bold" },
+    DiffText = { fg = C.none, bg = C.diff_text },
+    DiffAdded = { fg = C.green },
+    DiffRemoved = { fg = C.red },
+    DiffFile = { fg = C.cyan },
+    DiffIndexLine = { fg = C.gray },
 }
 
 return Diff
