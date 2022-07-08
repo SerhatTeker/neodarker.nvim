f287ec4f0e25d80c4db8a74c2e0b9eaa482d3f6a
Serhat Teker <serhat.teker@gmail.com>
2022-07-09 01:25:59 +0300

    Update VertSplit for more transparency

--- lua/neodarker/highlights.lua
+++ lua/neodarker/highlights.lua
@@ -24,7 +24,7 @@ local common = {
     LineNr = { fg = C.context },
     FloatBorder = { fg = C.gray, bg = C.alt_bg },
     Whitespace = { fg = C.gray },
-    VertSplit = { fg = C.bg, bg = C.fg },
+    VertSplit = { fg = C.bg, bg = C.dark_gray }, -- alt: context
     CursorLine = { bg = C.dark },
     CursorColumn = { bg = C.dark },
     ColorColumn = { bg = C.dark },
